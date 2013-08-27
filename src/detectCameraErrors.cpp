743d765ea21f47baf9961a2b1375709a6d2d1b46
Henrik Skov Midtiby <henrik@henrik-HP-EliteBook-8570w.(none)>
2013-08-27 16:13:05 +0200

    Code cleanup.

--- src/detectCameraErrors.cpp
+++ src/detectCameraErrors.cpp
@@ -4,7 +4,6 @@
 error_detector::error_detector(void )
 {
     errorCounter = 0;
-    std::cout << "previousImage.rows = " << previousImage.rows << std::endl;
 }
 
 error_detector::~error_detector()
@@ -13,17 +12,15 @@ error_detector::~error_detector()
 
 void error_detector::newBayerGRImage(cv::Mat img, qint64 timestamp)
 {
-//     std::cout << "<error_detector::newBayerGRImage />" << std::endl;
+    // Method for comparing timewise adjacent images and spot similar regions in these images.
     cv::Mat BayerGR8;
     QTGIGE::convert16to8bit(img, BayerGR8);
     if(previousImage.rows > 0) 
     {
-  	tempImage = 10*abs(BayerGR8 - previousImage) + 10*abs(previousImage - BayerGR8);
+	// An earlier image was stored, so calculate the absolute difference and 
+	// emit a signal with the difference image.
+	tempImage = 10*abs(BayerGR8 - previousImage) + 10*abs(previousImage - BayerGR8);
 	emit(newImage(tempImage, timestamp));    
-    }
-    else
-    {
-	std::cout << "Creating previousImage and tempImage." << std::endl;
     }
     previousImage = BayerGR8.clone();
 }
